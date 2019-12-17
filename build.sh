d6369daa7d2b792218bb08f892293f105370462c
Tennana <tennana@tennana.net>
2019-12-18 03:06:48 +0900

    fix: URLで引っかかってそうなので区切り文字を変更

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-sed "s/{@@WEBHOOKURL@@}/${WEB_HOOK_URL}/" index_base.html > index.html
-sed "s/{@@CACHENAME@@}/${COMMIT_REF}/" serviceworker_base.js > serviceworker.js
+sed "s|{@@WEBHOOKURL@@}|${WEB_HOOK_URL}|g" index_base.html > index.html
+sed "s|{@@CACHENAME@@}|${COMMIT_REF}|g" serviceworker_base.js > serviceworker.js
